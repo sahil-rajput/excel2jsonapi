6da4f44176835403ce289b7152adfd892e135e91
Sahil Rajput <sahil@Sahils-MacBook-Pro-2.local>
2020-04-25 14:29:51 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='excel2jsonapi',
+      version='0.1',
+      description='Convert excel into json response',
+      url='https://github.com/sahil-rajput/excel2jsonapi',
+      author='Sahil Rajput',
+      author_email='sahil.rajput.0196@gmail.com',
+      license='GNU',
+      packages=['excel2jsonapi'],
+      zip_safe=False)
